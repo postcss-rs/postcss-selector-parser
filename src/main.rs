31c406d37c787ed719fa649c2bd4bbf8f314bbac
CGQAQ <m.jason.liu@outlook.com>
2021-11-21 18:24:16 +0800

    add bench
    Signed-off-by: CGQAQ <m.jason.liu@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use plugin_selector_parser::tokenizer;
+use std::time;
 
 fn main() {
-    let input = "::v-deep( .foo > #foo div )";
-    let mut t = tokenizer::Tokenizer::new(input, None);
+    let t1 = time::Instant::now();
+
+    let x = " .foo > #foo div ".repeat(1_000_000);
+
+    let input = format!("::v-deep({})", x);
+    let mut t = tokenizer::Tokenizer::new(&input, None);
     let tokens = t.tokenize();
 
-    for t in tokens.iter() {
-        print!("{}", t.to_string());
-        println!("{}", &input[t.pos.0..t.pos.1])
-    }
+    println!("{}s", (t1.elapsed().as_millis() as f32) / 1000f32);
+
+    // for t in tokens.iter() {
+    //     print!("{}", t.to_string());
+    //     println!("{}", &input[t.pos.0..t.pos.1])
+    // }
 
     drop(tokens)
 }
